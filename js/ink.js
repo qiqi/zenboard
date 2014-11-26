fa91d7937598fe9a994397623107e536fc3bf4f1
Qiqi Wang <qiqi@mit.edu>
2014-11-26 15:06:14 -0500

    first commit

---
+++ js/ink.js
@@ -0,0 +1,101 @@
+﻿(function () {
+    // UI object references.
+    console.log('sdfdsfsadf');
+    var inkCanvas;
+    var inkContext;
+    strokes = {};
+    var ref = firebaseRef.child('test-pres').child('ink');
+
+    // Initial pointer values.
+    var pointerId = -1;
+    var pointerDeviceType = null;
+
+    // Initial stroke property values.
+    var strokeColor;
+    var strokeWidth;
+
+    //
+    // End global variables
+    //
+
+    // Obtain reference to the specified element.
+    function get(elementId) {
+        return document.getElementById(elementId);
+    }
+
+    $(document).ready(function() {
+        // Set up the UI.
+        inkCanvas = get("inkCanvas");
+        inkContext = inkCanvas.getContext("2d");
+
+        inkContext.lineCap = "round";
+        inkContext.lineJoin = "round";
+
+        inkCanvas.width = window.innerWidth;
+        inkCanvas.height = window.innerWidth;
+
+        ref.on('value', function(snapshot) {
+            inkContext.clearRect(0, 0, inkCanvas.width, inkCanvas.height);
+            var strokes = snapshot.val();
+            console.log(strokes);
+            for (id in strokes) {
+                renderSerializedStroke(strokes[id]);
+            }
+        });
+    });
+
+    function deserializeStrokeData(data) {
+        // function serializePixel(x) {
+        //     return Math.round(x / inkCanvas.width * 40000).toString(36);
+        // }
+        function deserializePixel(x) {
+            return Number.parseInt(x, 36) / 40000 * inkCanvas.width;
+        }
+        function deserializeSegment(seg) {
+            seg = seg.split(':');
+            return {
+                pressure: deserializePixel(seg[0]),
+                points: seg[1].split(',').map(deserializePoint)
+            };
+        }
+        function deserializePoint(point) {
+            var pos = point.split('_');
+            return {
+                x: deserializePixel(pos[0]),
+                y: deserializePixel(pos[1]),
+            };
+        }
+        segments = data.split(';').slice(0, -1);
+        return segments.map(deserializeSegment);
+    }
+
+    function renderSerializedStroke(stroke) {
+        inkContext.beginPath();
+        // Enumerate through each line segment of the stroke.
+        var first = true;
+        inkContext.strokeStyle = stroke.color;
+        deserializeStrokeData(stroke.data).forEach(
+            function (segment) {
+                // Move to the starting screen location of the stroke.
+                inkContext.lineWidth = segment.pressure * 3;
+                if (first) {
+                    inkContext.moveTo(segment.points[0].x, segment.points[0].y);
+                    first = false;
+                }
+                    // Calculate the bezier curve for the segment.
+                else {
+                    inkContext.bezierCurveTo(segment.points[0].x,
+                                             segment.points[0].y,
+                                             segment.points[1].x,
+                                             segment.points[1].y,
+                                             segment.points[2].x,
+                                             segment.points[2].y);
+                    inkContext.stroke();
+                    inkContext.closePath();
+                    inkContext.beginPath();
+                    inkContext.moveTo(segment.points[2].x, segment.points[2].y);
+                }
+            }
+        );
+    }
+})();
---
+++ js/zenBoard.js
@@ -0,0 +1,38 @@
+﻿Firebase.INTERNAL.forceWebSockets();
+var firebaseRef = new Firebase("https://infinite-walls.firebaseio.com/pres");
+
+angular.module('zenBoard', ['firebase']).controller('boardCtrl', function ($scope, $sce, $firebase) {
+    $scope.Math = window.Math;
+    $scope.$sce = $sce;
+
+    $scope.pres = {};
+    $scope.slide = {};
+    $firebase(firebaseRef.child('test-pres')).$asObject().$bindTo($scope, 'pres');
+
+    $scope.$watch(function () {
+        return $scope.pres && $scope.pres.slides && $scope.pres.selectedSlideId
+            && $scope.pres.slides[$scope.pres.selectedSlideId];
+    }, function (slide) {
+        if (slide) {
+            $scope.slide = slide;
+        }
+    })
+
+    $scope.$watch(function () {
+        return $scope.slide.url;
+    }, function (url) {
+        console.log(url);
+        if (url && url.indexOf('http://') != 0 && url.indexOf('https://') != 0) {
+            url = 'http://' + url;
+        }
+        try {
+            $('#webview').attr('src', url);
+        } catch (err) {
+            $('#webview').attr('src', 'about:blank');
+        }
+    });
+
+    $scope.selectSlide = function (slideId) {
+        $scope.pres.selectedSlideId = slideId;
+    }
+});
